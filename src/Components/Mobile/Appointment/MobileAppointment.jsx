1e691ced00c1aafa948ab5262a2401b3be4699e5
Partha Pratim Mazumdar <parthageek1@gmail.com>
2024-06-10 20:30:20 +0530

    appointment page done

--- src/Components/Mobile/Appointment/MobileAppointment.jsx
+++ src/Components/Mobile/Appointment/MobileAppointment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MobileNavbar from '../MobileNavbar/MobileNavbar'
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { useNavigate } from 'react-router-dom';
@@ -6,10 +6,15 @@ import { doctorData } from './doctorData';
 
 const MobileAppointment = () => {
     const navigate = useNavigate();
+    const [selectedTime, setSelectedTime] = useState(null);
+    const [appointmentType, setAppointmentType] = useState("");
+    const [description, setDescription] = useState("");
+   
+    const handlesubmit = (e)=>{
+        e.preventDefault();
+    }
 
-    // const handlesubmit = (e)=>{
-    //     e.preventDefault();
-    // }
+    const times = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"];
   return (
     <>
       <MobileNavbar />
@@ -27,8 +32,9 @@ const MobileAppointment = () => {
           </div>
         </div>
         {/* form */}
-        <div>
-          <form className="mx-5 pt-10">
+        <div className="pb-32">
+          <form onSubmit={handlesubmit} className="mx-5  pt-16">
+            {/* Name */}
             <label className="block font-poppins-medium flex pb-3 text-gray-700">
               Name*
             </label>
@@ -40,7 +46,8 @@ const MobileAppointment = () => {
               placeholder="Patient name"
               required
             />
-            <div className="flex pt-3 gap-3 ">
+            <div className="flex pt-7 gap-3 ">
+              {/* Age */}
               <div className="">
                 <label className="block font-poppins-medium flex pb-3 text-gray-700">
                   Age*
@@ -54,13 +61,13 @@ const MobileAppointment = () => {
                   required
                 />
               </div>
-
+              {/* Gender */}
               <div className="ml-5">
                 <label className="block font-poppins-medium flex pb-3 text-gray-700">
                   Gender*
                 </label>
                 <select
-                  className=" px-5  flex  border py-4 w-full  rounded-xl focus:outline-blue-500"
+                  className=" px-5 font-poppins-regular  flex  border py-4 w-full  rounded-xl focus:outline-blue-500"
                   id="gender"
                   name="gender"
                   required
@@ -74,15 +81,16 @@ const MobileAppointment = () => {
                 </select>
               </div>
             </div>
-            <div>
+            {/* doctor select */}
+            <div className="pt-7">
               <label
-                className="block pt-3 font-poppins-medium flex pb-2 text-gray-700"
+                className="block  font-poppins-medium flex pb-2 text-gray-700"
                 htmlFor="doctor"
               >
                 Doctor*
               </label>
               <select
-                className="px-5  flex  border py-4 w-full  rounded-xl focus:outline-blue-500"
+                className="px-5 text-sm font-poppins-regular flex  border py-4 w-full  rounded-xl focus:outline-blue-500"
                 id="doctor"
                 name="doctor"
                 required
@@ -96,6 +104,98 @@ const MobileAppointment = () => {
                   </option>
                 ))}
               </select>
+            </div>
+            {/* Time select */}
+            <div className="pb-5 pt-7">
+              <label className="font-poppins-medium flex pb-2 text-gray-700">
+                Select Appointment Time*
+              </label>
+              <div className="grid grid-cols-4 text-xs gap-2">
+                {times.map((time) => (
+                  <button
+                    type="button"
+                    key={time}
+                    className={`px-4 py-3 font-poppins-regular border-none rounded-3xl ${
+                      selectedTime === time
+                        ? "bg-blue-500 text-white"
+                        : "bg-white text-gray-700 border border-gray-300"
+                    }`}
+                    onClick={() => setSelectedTime(time)}
+                  >
+                    {time}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            {/* Appointment type */}
+            <div>
+              <label className="block  font-poppins-medium flex pb-2 text-gray-700">
+                Select Appointment Type*
+              </label>
+              <div className="flex flex-col space-y-2">
+                <label
+                  className={`block pl-5 flex  py-5 w-full font-poppins-regular  rounded-xl focus:outline-blue-500 ${
+                    appointmentType === "online"
+                      ? "bg-blue-500 text-white"
+                      : "bg-white text-gray-700 "
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    name="appointmentType"
+                    value="online"
+                    className="hidden"
+                    checked={appointmentType === "online"}
+                    onChange={(e) => setAppointmentType(e.target.value)}
+                  />
+                  Online Appointment
+                </label>
+                <label
+                  className={`block pl-5 flex  py-5 font-poppins-regular w-full  rounded-xl focus:outline-blue-500 ${
+                    appointmentType === "care"
+                      ? "bg-blue-500 text-white"
+                      : "bg-white text-gray-700"
+                  }`}
+                >
+                  <input
+                    type="radio"
+                    name="appointmentType"
+                    value="care"
+                    className="hidden"
+                    checked={appointmentType === "care"}
+                    onChange={(e) => setAppointmentType(e.target.value)}
+                  />
+                  Care Visit Appointment
+                </label>
+              </div>
+            </div>
+            {/* Describe */}
+            <div className=" pt-7">
+              <label className="block font-poppins-medium flex pb-3 text-gray-700">
+                Describe your disease*
+              </label>
+              <textarea
+                className="w-full px-3 py-2  rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+                id="description"
+                name="description"
+                rows="4"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
+            </div>
+            {/* Button */}
+            <div className="">
+              <div className=" flex items-center justify-center">
+                <button
+                  type="submit"
+                  onClick={() => navigate("/appointment")}
+                  className="py-5 w-full flex items-center justify-center bg-blue-500 border rounded-2xl mt-7 flex text-white text-xl font-jost-medium "
+                >
+                  Set apoointment
+                </button>
+              </div>
             </div>
           </form>
         </div>
